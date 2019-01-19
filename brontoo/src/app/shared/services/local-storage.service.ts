be0243effdebf11ca76651ddc767a4bed940ce5f
kaos2404 <kaos2404@users.noreply.github.com>
2019-01-20 02:18:00 +0530

    Final build

--- brontoo/src/app/shared/services/local-storage.service.ts
+++ brontoo/src/app/shared/services/local-storage.service.ts
@@ -3,8 +3,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class LocalStorageService {
-  idleTime: number = 20;
-  timeoutTime: number = 10;
+  idleTime: number = 60;
+  timeoutTime: number = 30;
   options: string[] = ['One', 'Two', 'Three'];
   emailPattern=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   numberPattern = /^[7-9][0-9]{9}$/;
